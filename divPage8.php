8c945568757eab11acf15b8cb21b4cc438604f94
EliteTools <elitetoolsife@gmail.com>
2025-02-11 03:19:08 +0100

    Update divPage8.php

--- divPage8.php
+++ divPage8.php
@@ -2,8 +2,10 @@
 ob_start();
 session_start();
 date_default_timezone_set('UTC');
-include "../includes/config.php";
-
+require_once "includes/config.php";
+require_once "templates/header.php";
+require_once "templates/style.php";
+require_once "templates/navbar.php";
 if (!isset($_SESSION['sname']) && !isset($_SESSION['spass'])) {
     header("location: ../");
     exit();
