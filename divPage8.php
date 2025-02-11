562fe338d04f0fea40ad4be45f27c21d14f5462c
EliteTools <elitetoolsife@gmail.com>
2025-02-11 03:47:25 +0100

    Update divPage8.php

--- divPage8.php
+++ divPage8.php
@@ -20,11 +20,11 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
     <div class="tab-pane active in" id="filter">
         <div class="filter-container">
             <div class="filter-row">
-                <label for="huntington_country">Country:</label>
-                <select class="filterselect form-control input-sm" name="huntington_country" id="huntington_country">
+                <label for="hungtingtonbanks_country">Country:</label>
+                <select class="filterselect form-control input-sm" name="hungtingtonbanks_country" id="hungtingtonbanks_country">
                     <option value="">ALL</option>
                     <?php
-                    $query = mysqli_query($dbcon, "SELECT DISTINCT `country` FROM `huntington` WHERE `sold` = '0' ORDER BY country ASC");
+                    $query = mysqli_query($dbcon, "SELECT DISTINCT `country` FROM `hungtingtonbanks` WHERE `sold` = '0' ORDER BY country ASC");
                     while ($row = mysqli_fetch_assoc($query)) {
                         echo '<option value="' . htmlspecialchars($row['country']) . '">' . htmlspecialchars($row['country']) . '</option>';
                     }
@@ -32,15 +32,15 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
                 </select>
             </div>
             <div class="filter-row">
-                <label for="huntington_sitename">Site Name:</label>
-                <input class="filterinput form-control input-sm" name="huntington_sitename" id="huntington_sitename" size="3">
+                <label for="hungtingtonbanks_sitename">Site Name:</label>
+                <input class="filterinput form-control input-sm" name="hungtingtonbanks_sitename" id="hungtingtonbanks_sitename" size="3">
             </div>
             <div class="filter-row">
-                <label for="huntington_seller">Seller:</label>
-                <select class="filterselect form-control input-sm" name="huntington_seller" id="huntington_seller">
+                <label for="hungtingtonbanks_seller">Seller:</label>
+                <select class="filterselect form-control input-sm" name="hungtingtonbanks_seller" id="hungtingtonbanks_seller">
                     <option value="">ALL</option>
                     <?php
-                    $query = mysqli_query($dbcon, "SELECT DISTINCT `resseller` FROM `huntington` WHERE `sold` = '0' ORDER BY resseller ASC");
+                    $query = mysqli_query($dbcon, "SELECT DISTINCT `resseller` FROM `hungtingtonbanks` WHERE `sold` = '0' ORDER BY resseller ASC");
                     while ($row = mysqli_fetch_assoc($query)) {
                         $qer = mysqli_query($dbcon, "SELECT DISTINCT `id` FROM resseller WHERE username='" . mysqli_real_escape_string($dbcon, $row['resseller']) . "' ORDER BY id ASC") or die(mysqli_error($dbcon));
                         while ($rpw = mysqli_fetch_assoc($qer)) {
@@ -74,7 +74,7 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
     <tbody>
         <?php
         include("cr.php");
-        $q = mysqli_query($dbcon, "SELECT * FROM huntington WHERE sold='0' ORDER BY RAND()")or die(mysqli_error());
+        $q = mysqli_query($dbcon, "SELECT * FROM hungtingtonbanks WHERE sold='0' ORDER BY RAND()")or die(mysqli_error());
         while ($row = mysqli_fetch_assoc($q)) {
             $countryfullname = htmlspecialchars($row['country']);
             $code = array_search("$countryfullname", $countrycodes);
@@ -84,13 +84,13 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
                 $SellerNick = "seller" . htmlspecialchars($rpw["id"]);
                 echo "
                 <tr>
-                    <td id='huntington_country'><i class='flag-icon flag-icon-$countrycode'></i>&nbsp;" . htmlspecialchars($row['country']) . "</td>
+                    <td id='hungtingtonbanks_country'><i class='flag-icon flag-icon-$countrycode'></i>&nbsp;" . htmlspecialchars($row['country']) . "</td>
                     <td><span type='button' class='badge badge-success' title='Linux a2plcpnl0739.prod.iad2.secureserver.net 2.6.32-954.3.5.lve1.4.92.el6.x86_64 #1 SMP Tue Jul 4 15:05:25 UTC 2023 x86'><i class='fab fa-linux'></i> " . htmlspecialchars($row['bankname']) . "</span><span class='alert alert-warning det-host-info' style='display:none'>Linux a2plcpnl0739.prod.iad2.secureserver.net 2.6.32-954.3.5.lve1.4.92.el6.x86_64 #1 SMP Tue Jul 4 15:05:25 UTC 2023 x86</span></td>
                     <td><span type='button' class='badge badge-warning' title='Linux a2plcpnl0739.prod.iad2.secureserver.net 2.6.32-954.3.5.lve1.4.92.el6.x86_64 #1 SMP Tue Jul 4 15:05:25 UTC 2023 x86'><i class='fab fa-linux'></i> $" . htmlspecialchars($row['balance']) . "</span><span class='alert alert-dark det-host-info' style='display:none'>Linux a2plcpnl0739.prod.iad2.secureserver.net 2.6.32-954.3.5.lve1.4.92.el6.x86_64 #1 SMP Tue Jul 4 15:05:25 UTC 2023 x86</span></td>
                     <td>" . htmlspecialchars($row['infos']) . "</td>
                     <td><span type='button' class='badge badge-dark' title='Linux a2plcpnl0739.prod.iad2.secureserver.net 2.6.32-954.3.5.lve1.4.92.el6.x86_64 #1 SMP Tue Jul 4 15:05:25 UTC 2023 x86'> $" . htmlspecialchars($row['price']) . "</span><span class='alert alert-dark det-host-info' style='display:none'>Linux a2plcpnl0739.prod.iad2.secureserver.net 2.6.32-954.3.5.lve1.4.92.el6.x86_64 #1 SMP Tue Jul 4 15:05:25 UTC 2023 x86</span></td>
                     <td>" . $row['date'] . "</td>
-                    <td><span id='huntington{$row['id']}' title='buy' type='huntington'><a onclick='buythistool({$row['id']})' class='btn btn-primary btn-xs'><font color=white>Buy</font></a></span></td>
+                    <td><span id='hungtingtonbanks{$row['id']}' title='buy' type='hungtingtonbanks'><a onclick='buythistool({$row['id']})' class='btn btn-primary btn-xs'><font color=white>Buy</font></a></span></td>
                 </tr>
                 ";
             }
@@ -114,9 +114,9 @@ $(document).ready(function() {
             var ck1 = $.trim(data[0].toLowerCase());
             var ck2 = $.trim(data[1].toLowerCase());
             var ck3 = $.trim(data[2].toLowerCase());
-            var fk1 = $.trim($("#huntington_country").val().toLowerCase());
-            var fk2 = $.trim($("#huntington_sitename").val().toLowerCase());
-            var fk3 = $.trim($("#huntington_seller").val().toLowerCase());
+            var fk1 = $.trim($("#hungtingtonbanks_country").val().toLowerCase());
+            var fk2 = $.trim($("#hungtingtonbanks_sitename").val().toLowerCase());
+            var fk3 = $.trim($("#hungtingtonbanks_seller").val().toLowerCase());
 
             if ((fk1 === "" || ck1 === fk1) && (fk2 === "" || ck2.includes(fk2)) && (fk3 === "" || ck3 === fk3)) {
                 return true;
@@ -144,14 +144,14 @@ function buythistool(id) {
                 didOpen: () => {
                     Swal.showLoading()
                     $.ajax({
-                        url: `buy/huntington.php?id=${id}`,
+                        url: `buy/hungtingtonbanks.php?id=${id}`,
                         method: 'GET',
                         dataType: 'json',
                         success: function(data) {
                             Swal.close();
                             if (data.status === 'success') {
                                 Swal.fire('Success!', 'Order placed successfully!', 'success').then(() => {
-                                    $("#huntington" + id).html('<button onclick="openitem(' + data.order_id + ')" class="btn btn-primary btn-xs"> Order #' + data.order_id + '</button>');
+                                    $("#hungtingtonbanks" + id).html('<button onclick="openitem(' + data.order_id + ')" class="btn btn-primary btn-xs"> Order #' + data.order_id + '</button>');
                                 });
                             } else {
                                 Swal.fire('Error!', data.message, 'error');
