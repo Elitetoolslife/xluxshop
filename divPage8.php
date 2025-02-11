ba4fd7b939482e177608e62b22143cb80949fcf4
EliteTools <elitetoolsife@gmail.com>
2025-02-11 03:18:14 +0100

    Update divPage8.php

--- divPage8.php
+++ divPage8.php
@@ -2,9 +2,9 @@
 ob_start();
 session_start();
 date_default_timezone_set('UTC');
-include "includes/config.php";
+include "../includes/config.php";
 
-if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
+if (!isset($_SESSION['sname']) && !isset($_SESSION['spass'])) {
     header("location: ../");
     exit();
 }
@@ -12,126 +12,180 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 ?>
 
 <ul class="nav nav-tabs">
-  <li class="active"><a href="#filter" data-toggle="tab">Filter</a></li>
+    <li class="active"><a href="#filter" data-toggle="tab">Filter</a></li>
 </ul>
-<div id="myTabContent" class="tab-content" >
-  <div class="tab-pane active in" id="filter"><table class="table"><thead><tr><th>Country</th>
-<th>Site Name</th>
-<th>Seller</th>
-<th></th></tr></thead><tbody><tr><td><select class='filterselect form-control input-sm' name="bank_country"><option value="">ALL</option>
-<?php
-$query = mysqli_query($dbcon, "SELECT DISTINCT(`country`) FROM `banks` WHERE `sold` = '0' ORDER BY country ASC");
-	while($row = mysqli_fetch_assoc($query)){
-	echo '<option value="'.$row['country'].'">'.$row['country'].'</option>';
-	}
-?>
-</select></td><td><input class='filterinput form-control input-sm' name="bank_sitename" size='3'></td><td><select class='filterselect form-control input-sm' name="bank_seller"><option value="">ALL</option>
-<?php
-$query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `banks` WHERE `sold` = '0' ORDER BY resseller ASC");
-	while($row = mysqli_fetch_assoc($query)){
-		 $qer = mysqli_query($dbcon, "SELECT DISTINCT(`id`) FROM resseller WHERE username='".$row['resseller']."' ORDER BY id ASC")or die(mysql_error());
-		   while($rpw = mysqli_fetch_assoc($qer))
-			 $SellerNick = "seller".$rpw["id"]."";
-	echo '<option value="'.$SellerNick.'">'.$SellerNick.'</option>';
-	}
-?>
-</select></td><td><button id='filterbutton'class="btn btn-primary btn-sm" disabled>Filter <span class="glyphicon glyphicon-filter"></span></button></td></tr></tbody></table></div>
+<div id="myTabContent" class="tab-content">
+    <div class="tab-pane active in" id="filter">
+        <div class="filter-container">
+            <div class="filter-row">
+                <label for="huntington_country">Country:</label>
+                <select class="filterselect form-control input-sm" name="huntington_country" id="huntington_country">
+                    <option value="">ALL</option>
+                    <?php
+                    $query = mysqli_query($dbcon, "SELECT DISTINCT `country` FROM `huntingtons` WHERE `sold` = '0' ORDER BY country ASC");
+                    while ($row = mysqli_fetch_assoc($query)) {
+                        echo '<option value="' . htmlspecialchars($row['country']) . '">' . htmlspecialchars($row['country']) . '</option>';
+                    }
+                    ?>
+                </select>
+            </div>
+            <div class="filter-row">
+                <label for="huntington_sitename">Site Name:</label>
+                <input class="filterinput form-control input-sm" name="huntington_sitename" id="huntington_sitename" size="3">
+            </div>
+            <div class="filter-row">
+                <label for="huntington_seller">Seller:</label>
+                <select class="filterselect form-control input-sm" name="huntington_seller" id="huntington_seller">
+                    <option value="">ALL</option>
+                    <?php
+                    $query = mysqli_query($dbcon, "SELECT DISTINCT `resseller` FROM `huntingtons` WHERE `sold` = '0' ORDER BY resseller ASC");
+                    while ($row = mysqli_fetch_assoc($query)) {
+                        $qer = mysqli_query($dbcon, "SELECT DISTINCT `id` FROM resseller WHERE username='" . mysqli_real_escape_string($dbcon, $row['resseller']) . "' ORDER BY id ASC") or die(mysqli_error($dbcon));
+                        while ($rpw = mysqli_fetch_assoc($qer)) {
+                            $SellerNick = "seller" . htmlspecialchars($rpw["id"]);
+                            echo '<option value="' . $SellerNick . '">' . $SellerNick . '</option>';
+                        }
+                    }
+                    ?>
+                </select>
+            </div>
+            <div class="filter-row">
+                <button id="filterbutton" class="btn btn-primary btn-sm" disabled>Filter</button>
+            </div>
+        </div>
+    </div>
 </div>
 
+<table id="table" class="display">
+    <thead>
+        <tr>
+            <th>Country</th>
+            <th>Site Name</th>
+            <th>Seller</th>
+            <th>Balance</th>
+            <th>Available Information</th>
+            <th>Price</th>
+            <th>Added on</th>
+            <th>Buy</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        include("cr.php");
+        $q = mysqli_query($dbcon, "SELECT * FROM huntingtons WHERE sold='0' ORDER BY RAND()")or die(mysqli_error());
+        while ($row = mysqli_fetch_assoc($q)) {
+            $countryfullname = htmlspecialchars($row['country']);
+            $code = array_search("$countryfullname", $countrycodes);
+            $countrycode = strtolower($code);
+            $qer = mysqli_query($dbcon, "SELECT * FROM resseller WHERE username='" . mysqli_real_escape_string($dbcon, $row['resseller']) . "'") or die(mysqli_error($dbcon));
+            while ($rpw = mysqli_fetch_assoc($qer)) {
+                $SellerNick = "seller" . htmlspecialchars($rpw["id"]);
+                echo "
+                <tr>
+                    <td id='huntington_country'><i class='flag-icon flag-icon-$countrycode'></i>&nbsp;" . htmlspecialchars($row['country']) . "</td>
+                    <td><span type='button' class='badge badge-success' title='Linux a2plcpnl0739.prod.iad2.secureserver.net 2.6.32-954.3.5.lve1.4.92.el6.x86_64 #1 SMP Tue Jul 4 15:05:25 UTC 2023 x86'><i class='fab fa-linux'></i> " . htmlspecialchars($row['bankname']) . "</span><span class='alert alert-warning det-host-info' style='display:none'>Linux a2plcpnl0739.prod.iad2.secureserver.net 2.6.32-954.3.5.lve1.4.92.el6.x86_64 #1 SMP Tue Jul 4 15:05:25 UTC 2023 x86</span></td>
+                    <td><span type='button' class='badge badge-warning' title='Linux a2plcpnl0739.prod.iad2.secureserver.net 2.6.32-954.3.5.lve1.4.92.el6.x86_64 #1 SMP Tue Jul 4 15:05:25 UTC 2023 x86'><i class='fab fa-linux'></i> $" . htmlspecialchars($row['balance']) . "</span><span class='alert alert-dark det-host-info' style='display:none'>Linux a2plcpnl0739.prod.iad2.secureserver.net 2.6.32-954.3.5.lve1.4.92.el6.x86_64 #1 SMP Tue Jul 4 15:05:25 UTC 2023 x86</span></td>
+                    <td>" . htmlspecialchars($row['infos']) . "</td>
+                    <td><span type='button' class='badge badge-dark' title='Linux a2plcpnl0739.prod.iad2.secureserver.net 2.6.32-954.3.5.lve1.4.92.el6.x86_64 #1 SMP Tue Jul 4 15:05:25 UTC 2023 x86'> $" . htmlspecialchars($row['price']) . "</span><span class='alert alert-dark det-host-info' style='display:none'>Linux a2plcpnl0739.prod.iad2.secureserver.net 2.6.32-954.3.5.lve1.4.92.el6.x86_64 #1 SMP Tue Jul 4 15:05:25 UTC 2023 x86</span></td>
+                    <td>" . $row['date'] . "</td>
+                    <td><span id='huntington{$row['id']}' title='buy' type='huntington'><a onclick='buythistool({$row['id']})' class='btn btn-primary btn-xs'><font color=white>Buy</font></a></span></td>
+                </tr>
+                ";
+            }
+        }
+        ?>
+    </tbody>
+</table>
 
-<table width="100%"  class="table table-striped table-bordered table-condensed sticky-header" id="table">
-<thead>
-    <tr>
-      <th scope="col" >Country</th>
-      <th scope="col">Bank Name</th>
-      <th scope="col">Balance</th>
-      <th scope="col">Available Information</th>
-      <th scope="col">Seller</th>
-      <th scope="col">Price</th>
-      <th scope="col">Added on </th>
-      <th scope="col">Buy</th>
-    </tr>
-</thead>
-  <tbody>
-
- <?php
-include("cr.php");
-$q = mysqli_query($dbcon, "SELECT * FROM banks WHERE sold='0' ORDER BY RAND()")or die(mysqli_error());
- while($row = mysqli_fetch_assoc($q)){
-	 
-	 	 $countryfullname = $row['country'];
-	  $code = array_search("$countryfullname", $countrycodes);
-	 $countrycode = strtolower($code);
-	    $qer = mysqli_query($dbcon, "SELECT * FROM resseller WHERE username='".$row['resseller']."'")or die(mysql_error());
-		   while($rpw = mysqli_fetch_assoc($qer))
-			 $SellerNick = "seller".$rpw["id"]."";
-     echo "
- <tr>     
-    <td id='bank_country'><i class='flag-icon flag-icon-$countrycode'></i>&nbsp;".htmlspecialchars($row['country'])." </td>
-    <td id='bank_sitename'> ".htmlspecialchars($row['bankname'])." </td> 
-    <td> ".htmlspecialchars($row['balance'])." </td> 
-	<td> ".htmlspecialchars($row['infos'])." </td>
-    <td id='bank_seller'> ".htmlspecialchars($SellerNick)."</td>
-    <td> ".htmlspecialchars($row['price'])."</td>
-	    <td> ".$row['date']."</td>";
-    echo '
-    <td>
-	<span id="bank'.$row['id'].'" title="buy" type="bank"><a onclick="javascript:buythistool('.$row['id'].')" class="btn btn-primary btn-xs"><font color=white>Buy</font></a></span><center>
-    </td>
-            </tr>
-     ';
- }
-
- ?>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 <script type="text/javascript">
-$('#filterbutton').click(function () {$("#table tbody tr").each(function() {var ck1 = $.trim( $(this).find("#bank_country").text().toLowerCase() );var ck2 = $.trim( $(this).find("#bank_sitename").text().toLowerCase() );var ck3 = $.trim( $(this).find("#bank_seller").text().toLowerCase() ); var val1 = $.trim( $('select[name="bank_country"]').val().toLowerCase() );var val2 = $.trim( $('input[name="bank_sitename"]').val().toLowerCase() );var val3 = $.trim( $('select[name="bank_seller"]').val().toLowerCase() ); if((ck1 != val1 && val1 != '' ) || ck2.indexOf(val2)==-1 || (ck3 != val3 && val3 != '' )){ $(this).hide();  }else{ $(this).show(); } });$('#filterbutton').prop('disabled', true);});$('.filterselect').change(function () {$('#filterbutton').prop('disabled', false);});$('.filterinput').keyup(function () {$('#filterbutton').prop('disabled', false);});
-function buythistool(id){
-  bootbox.confirm("Are you sure?", function(result) {
-        if(result ==true){
-      $.ajax({
-     method:"GET",
-     url:"buytool.php?id="+id+"&t=banks",
-     dataType:"text",
-     success:function(data){
-         if(data.match(/<button/)){
-		 $("#bank"+id).html(data).show();
-         }else{
-            bootbox.alert('<center><img src="files/img/balance.png"><h2><b>No enough balance !</b></h2><h4>Please refill your balance <a class="btn btn-primary btn-xs"  href="addBalance.html" onclick="window.open(this.href);return false;" >Add Balance <span class="glyphicon glyphicon-plus"></span></a></h4></center>')
-         }
-     },
-   });
-       ;}
-  });
+$(document).ready(function() {
+    $('#table').DataTable();
+
+    $('#filterbutton').click(function() {
+        var table = $('#table').DataTable();
+        table.draw();
+    });
+
+    $.fn.dataTable.ext.search.push(
+        function(settings, data, dataIndex) {
+            var ck1 = $.trim(data[0].toLowerCase());
+            var ck2 = $.trim(data[1].toLowerCase());
+            var ck3 = $.trim(data[2].toLowerCase());
+            var fk1 = $.trim($("#huntington_country").val().toLowerCase());
+            var fk2 = $.trim($("#huntington_sitename").val().toLowerCase());
+            var fk3 = $.trim($("#huntington_seller").val().toLowerCase());
+
+            if ((fk1 === "" || ck1 === fk1) && (fk2 === "" || ck2.includes(fk2)) && (fk3 === "" || ck3 === fk3)) {
+                return true;
+            }
+            return false;
+        }
+    );
+});
+
+function buythistool(id) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "Do you want to proceed with the purchase?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, buy it!',
+        cancelButtonText: 'No, cancel!',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: 'Processing...',
+                text: 'Please wait for 5 seconds...',
+                timer: 5000,
+                didOpen: () => {
+                    Swal.showLoading()
+                    $.ajax({
+                        url: `buy/huntington.php?id=${id}`,
+                        method: 'GET',
+                        dataType: 'json',
+                        success: function(data) {
+                            Swal.close();
+                            if (data.status === 'success') {
+                                Swal.fire('Success!', 'Order placed successfully!', 'success').then(() => {
+                                    $("#huntington" + id).html('<button onclick="openitem(' + data.order_id + ')" class="btn btn-primary btn-xs"> Order #' + data.order_id + '</button>');
+                                });
+                            } else {
+                                Swal.fire('Error!', data.message, 'error');
+                            }
+                        }
+                    });
+                }
+            });
+        }
+    });
 }
 
-function openitem(order){
-  $("#myModalLabel").text('Order #'+order);
-  $('#myModal').modal('show');
-  $.ajax({
-    type:       'GET',
-    url:        'showOrder'+order+'.html',
-    success:    function(data)
-    {
-        $("#modelbody").html(data).show();
-    }});
-
+function openitem(order) {
+    $("#myModalLabel").text(`Order #${order}`);
+    $('#myModal').modal('show');
+    $.ajax({
+        url: `showOrder${order}.html`,
+        method: 'GET',
+        success: function(data) {
+            $("#modelbody").html(data);
+        }
+    });
 }
-
 </script>
+
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel"></h4>
-      </div>
-      <div class="modal-body" id="modelbody">
-
-
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-      </div>
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel"></h4>
+            </div>
+            <div class="modal-body" id="modelbody"></div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
