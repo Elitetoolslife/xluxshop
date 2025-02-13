ccf5076f9a4e619e8f0886e4f936c8ea0bcbb635
EliteTools <elitetoolsife@gmail.com>
2025-02-13 19:26:43 +0100

    Update divPage8.php

--- divPage8.php
+++ divPage8.php
@@ -8,7 +8,7 @@ require 'vendor/autoload.php';
 use eftec\bladeone\BladeOne;
 use App\Models\User;
 
-class OrderController extends BaseController {
+class HuntingtonrController extends BaseController {
     private $blade;
     private $db;
     private $countrycodes;
